6ae3977b3cb19bb3e75e1b09393cc537ad516fbe
Harold Schreckengost <harold@haroldmschreckengost.com>
2018-11-19 19:15:36 -0600

    Dice rolling complete and tested
    Includes Die type, rolling, rolling multiple, and rolling highest and lowest.

---
+++ dice.go
@@ -0,0 +1,83 @@
+package drow
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"sort"
+	"time"
+	"errors"
+)
+
+// Representation of a die.  Mostly for doing methods on.
+type Die struct {
+	Randomizer rand.Rand
+	Sides int
+	ExpectedValue float64
+	Name string
+}
+
+// Generate a new die with specified number of sides
+func NewDie(sides int) *Die {
+	new_die := new(Die)
+	new_die.Sides = sides
+	new_die.Name = fmt.Sprintf("D%d", sides)
+	new_die.Randomizer = *rand.New(rand.NewSource(time.Now().UnixNano()))
+	new_die.ExpectedValue = float64((float64(new_die.Sides)+float64(1))/float64(2))
+	return new_die
+}
+
+// Roll a die
+func(die *Die) Roll() int {
+	return die.Randomizer.Intn(die.Sides) + 1
+}
+
+func (die *Die) RollMultiple(times int) []int {
+	rolls := make([]int, times)
+	for i := 0; i < times; i++ {
+		rolls[i] = die.Roll()
+	}
+	return rolls
+}
+
+// Takes number of times to roll, as well an integer for what to keep - negative numbers will keep lowest,
+// positive numbers will keep highest.  For example, -2 will keep the lowest 2, 2 will keep the highest two.
+func (die *Die) HighLow(times int, take int) (results []int, allRolls []int, err error) {
+	// Cannot take more than you roll, obviously.
+	if math.Abs(float64(take)) > float64(times) {
+		return make([]int, 0), make([]int, 0), errors.New("Cannot take more than rolled.")
+	}
+	if take == 0 {
+		return make([]int, 0), make([]int, 0), errors.New("Cannot take zero dice")
+	}
+	// Roll our dice "times" times and sort them in ascending order
+	rolls := die.RollMultiple(times)
+	sort.Ints(rolls)
+	// If we are taking
+	if take > 0 {
+		return rolls[len(rolls)-take:], rolls, nil
+	} else {
+		// Absolute value of take
+		take = 0 - take
+		return rolls[:take], rolls, nil
+	}
+}
+
+func (die *Die) Highest(times int, take int) (results []int, allRolls []int, err error) {
+	return die.HighLow(times, take)
+}
+
+func (die *Die) Lowest(times int, take int) (results []int, allRolls []int, err error) {
+	return die.HighLow(times, 0-take)
+}
+
+var D20 = NewDie(20)
+var D4  = NewDie(4)
+var D6 =  NewDie(6)
+var D8 =  NewDie(8)
+var D10 = NewDie(10)
+var D12 = NewDie(12)
+var D100 =NewDie(100)
+
+
+
---
+++ dice_test.go
@@ -0,0 +1,114 @@
+package drow
+
+import (
+	"fmt"
+	"math"
+	"testing"
+)
+
+var dice = []*Die{D4, D6, D8, D10, D12, D20, D100}
+const TestRolls = 100000
+const TestRollMaxVariance = 0.01
+
+// A basic check for appropriate randomness, based on a large distribution of rolls.  1% variance is... still pretty reasonable
+func TestDie_Roll(t *testing.T) {
+	for _, die := range dice {
+		expectedSum := die.ExpectedValue * float64(TestRolls)
+		sum := 0
+		for i := 0; i < TestRolls; i++ {
+			sum += die.Roll()
+		}
+		var variance  float64 = float64(math.Abs(expectedSum - float64(sum))/expectedSum)
+		if variance > TestRollMaxVariance {
+			t.Errorf(fmt.Sprintf("Die %s failed its test with a variance of %v", die.Name, variance))
+		}
+	}
+}
+
+const TestRollTimes = 10
+func TestDie_RollMultiple(t *testing.T) {
+	for _, die := range dice {
+		rolls := die.RollMultiple(TestRollTimes)
+		if len(rolls) != TestRollTimes {
+			t.Errorf(fmt.Sprintf("Die %s return %d results, expected %d", die.Name, len(rolls), TestRollTimes))
+		}
+	}
+}
+
+func TestDie_Highest(t *testing.T) {
+	for _, die := range dice {
+		timesToRoll := 4
+		take := 1
+		results, rolls, err := die.Highest(timesToRoll, take)
+		if err != nil {
+			t.Errorf("Encountered an error.")
+		}
+		if results[0] != rolls[len(rolls)-1] {
+			t.Errorf("Did not receive highest value.  Received %v, all results %v", results, rolls)
+		}
+		// test for correct multiple returns
+		take = 2
+		results, rolls, err = die.Highest(timesToRoll,  take)
+		if err != nil {
+			t.Errorf("Encountered an error.")
+		}
+		rollsEnd := rolls[len(rolls)-take:]
+		fmt.Println(results)
+		fmt.Println(rollsEnd)
+		for i := 0; i < take; i++ {
+			fmt.Println(i)
+			fmt.Println(rollsEnd[i])
+			if results[i] != rollsEnd[i] {
+				t.Errorf("Returned Results do not match highest.  %v vs %v", results, rolls)
+			}
+		}
+		// test for failure on invalid take
+		take = 0
+		_, _, err = die.Highest(timesToRoll, take)
+		if err == nil {
+			t.Errorf("Did not receive error on invalid input.")
+		}
+		take = timesToRoll+2
+		_, _, err = die.Highest(timesToRoll, take)
+		if err == nil {
+			t.Errorf("Did not receive error when take > times to roll")
+		}
+	}
+}
+
+func TestDie_Lowest(t *testing.T) {
+	for _, die := range dice {
+		timesToRoll := 4
+		take := 1
+		results, rolls, err := die.Lowest(timesToRoll, take)
+		if err != nil {
+			t.Errorf("Encountered an error.")
+		}
+		if results[0] != rolls[0] {
+			t.Errorf("Did not receive lowest value.  Received %v, all results %v", results, rolls)
+		}
+		// test for correct multiple returns
+		take = 2
+		results, rolls, err = die.Lowest(timesToRoll,  take)
+		if err != nil {
+			t.Errorf("Encountered an error.")
+		}
+		rollsStart := rolls[:take]
+		for i := 0; i < take; i++ {
+			if results[i] != rollsStart[i] {
+				t.Errorf("Returned Results do not match highest.  %v vs %v", results, rolls)
+			}
+		}
+		// test for failure on invalid take
+		take = 0
+		_, _, err = die.Lowest(timesToRoll, take)
+		if err == nil {
+			t.Errorf("Did not receive error on invalid input of 0.")
+		}
+		take = timesToRoll+2
+		_, _, err = die.Lowest(timesToRoll, take)
+		if err == nil {
+			t.Errorf("Did not receive error when take > times to roll")
+		}
+	}
+}
